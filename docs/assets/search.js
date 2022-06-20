218aa71d178957054e333f560ae176fa845811c0
zhiyuan <zhiyuan.hu@stud.tu-darmstadt.de>
2022-06-20 16:01:53 +0200

    update

--- docs/assets/search.js
+++ docs/assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"Welcome",'url':"/2018/07/01/welcome.html"},{'title':"岁月不居，时节如流",'url':"/2022/05/30/birthday.html"}]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':"Welcome",'url':"/2018/07/01/welcome.html"},{'title':"岁月不居，时节如流",'url':"/2022/05/30/birthday.html"},{'title':"笑吧，皮奥莱维奇",'url':"/2022/06/10/laugh.html"}]};
