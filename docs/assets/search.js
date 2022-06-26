03c841fd78a18c2bd91cb3e0f297ac80c3219ed5
zhiyuan <zhiyuan.hu@stud.tu-darmstadt.de>
2022-06-26 16:46:13 +0200

    test bilibili embeded

--- docs/assets/search.js
+++ docs/assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"Welcome",'url':"/2018/07/01/welcome.html"},{'title':"岁月不居，时节如流",'url':"/2022/05/30/birthday.html"},{'title':"笑吧，皮奥莱维奇",'url':"/2022/06/10/laugh.html"}]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':"Welcome",'url':"/2018/07/01/welcome.html"},{'title':"岁月不居，时节如流",'url':"/2022/05/30/birthday.html"},{'title':"笑吧，皮奥莱维奇",'url':"/2022/06/10/laugh.html"},{'title':"唐山笔记",'url':"/2022/06/20/TangShan.html"}]};
